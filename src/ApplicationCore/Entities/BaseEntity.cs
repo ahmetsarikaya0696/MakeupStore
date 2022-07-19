e6d97dca6e5431e9d6f1b9327561fc23bb8df4b7
ahmet sarikaya <ahmetsarikaya0696@gmail.com>
2022-07-19 13:20:54 +0300

    -Entities -IRepository -Specification

---
+++ src/ApplicationCore/Entities/BaseEntity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationCore.Entities
+{
+    public abstract class BaseEntity
+    {
+        // get ve set'i değiştirilebilir yapmak amacıyla virtual yapıldı 
+        public virtual int Id { get; set; }
+    }
+}

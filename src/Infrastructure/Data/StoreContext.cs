b2d648eeab1e2639c99e4fcc599a21938c747edf
ahmet sarikaya <ahmetsarikaya0696@gmail.com>
2022-07-19 14:46:11 +0300

    -StoreContext -Config

---
+++ src/Infrastructure/Data/StoreContext.cs
@@ -0,0 +1,33 @@
+﻿using ApplicationCore.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationCore.Interfaces.Data
+{
+    public class StoreContext : DbContext
+    {
+        public StoreContext(DbContextOptions<StoreContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // Bütün konfigürasyonları uygular.
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+
+        public DbSet<Category> Categories { get; set; }
+
+        public DbSet<Brand> Brands { get; set; }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}

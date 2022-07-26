7b773a66824aa31b4a4f8585b2ac8e88d874d9bc
ahmet sarikaya <ahmetsarikaya0696@gmail.com>
2022-07-26 13:51:58 +0300

    Basket, BasketItem, BasketConfiguration, DbSet<Basket>,DbSet<BasketItem>, migration

--- src/Infrastructure/Data/StoreContext.cs
+++ src/Infrastructure/Data/StoreContext.cs
@@ -29,5 +29,9 @@ namespace Infrastructure.Data
         public DbSet<Brand> Brands { get; set; }
 
         public DbSet<Product> Products { get; set; }
+
+        public DbSet<Basket> Baskets { get; set; }
+        
+        public DbSet<BasketItem> BasketItems { get; set; }
     }
 }
